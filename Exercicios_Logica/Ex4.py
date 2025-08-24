bc294ddbe93a2d218f82c37daab28f27ab555d8b
Pedrovicentte <pedrohenei092@gmail.com>
2025-08-24 11:06:21 -0300

    Atualização de Exercícios + POO python

--- Exercicios_Logica/Ex4.py
+++ Exercicios_Logica/Ex4.py
@@ -2,6 +2,10 @@ print("|----|Leitor de valores|----|")
 n1 = float(input("  Digite um valor: "))
 n2 = float(input("  Digite outro valor: "))
 
+print("minha primeira ramificação")
+print("minha primeira ramificação")
+print("minha primeira ramificação")
+
 if (n1 > n2):
     print(" O ", n1,"é o maior número")
 else:
---
+++ POO_pytho/main.py
@@ -0,0 +1,10 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 29)
+p2 = Pessoa('Joao', 45)
+
+p1.falar('POO')
+p2.falar('Filmes')
+p2.parar_falar()
+p1.comer('chirrasco')
+p2.comer('sorvete')
