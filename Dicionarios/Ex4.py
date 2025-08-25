88d1663c9e31da96438a7884b2372c8047e7d85b
Pedrovicentte <pedrohenei092@gmail.com>
2025-08-25 08:59:34 -0300

    atividade nao finalizada

--- Dicionarios/Ex4.py
+++ Dicionarios/Ex4.py
@@ -3,8 +3,25 @@ Peça ao usuário uma frase.
 Conte quantas vezes cada palavra aparece e guarde os resultados em um dicionário.
 '''
 
-'''frase = {
-   {} : []
 
-}'''
+frase ={
+
+}
+
+frase = input("Digite uma frase: ").split()
+valor = 0
+for chave in frase:
+  valor += 1
+  if chave in frase:
+      valor + 1
+      print(f"{frase} //// {valor}'")
+      ''' 
+    if chave:
+        valor += 1
+        print(f"Chave:{chave}: Valor:{valor}")  
+      ''''''
+#contar cada item da lista 
+'''
+
+
 
