f76fbee2b2659cb26ec40b06c85a1fe8aae8af7c
Pedrovicentte <pedrohenei092@gmail.com>
2025-09-13 17:49:34 -0300

    Exercicio finalizado

---
+++ Introducao_python/cap6/Ex6.3.py
@@ -0,0 +1,22 @@
+'''
+Faça um programa que percorra duas listas e gere uma terceira sem elementos repetidos.
+'''
+
+lista1 = [1,2,3,4,5]
+lista2 = [4,5,6,7,8]
+lista3 = []
+
+x = 0
+while x < len(lista1):
+ lista1[x] # lendo cada valor da lista1 na posição x 
+ lista3.append(lista1[x])
+ x+=1
+
+x = 0
+while x < len(lista2):
+ if lista2[x] not in lista3: 
+    lista3.append(lista2[x])       
+ x+=1
+print(lista3)
+
+
