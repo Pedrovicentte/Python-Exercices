f4591cdb9a8292bd23764f30dde507ea872ffab8
Pedro <pedrohenei092@gmail.com>
2026-04-20 08:24:29 -0300

    Aula e Exercício de Herança

---
+++ POO_pytho/POO-CursoEmVideo/Aula7/__main__.py
@@ -0,0 +1,19 @@
+from rich import print, inspect
+from aluno import Aluno
+from funcionario import Funcionario
+from professor import Professor
+
+def main():
+    a1 = Aluno("José", 17, "Informática", "T01")
+    a1.fazer_aniversário()
+    a1.fazer_matricula()
+
+    p1 = Professor("Samuel", 45, "Matématica", "Doutor")
+    p1.dar_aula()
+
+    f1 = Funcionario("Claudia", 29, "Secretária", "Financeiro")
+    a1.fazer_aniversário()
+    f1.bater_ponto()
+
+if __name__ == "__main__":
+    main()
